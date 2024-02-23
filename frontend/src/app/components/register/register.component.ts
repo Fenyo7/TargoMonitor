3bfec4235a063710008cf13dd0e543a82ac12183
Fenyo7 <fenyo.lev@gmail.com>
2024-02-23 15:19:17 +0100

    add(fe): register input validation

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -16,13 +16,60 @@ export class RegisterComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       name: ['', [Validators.required]],
       password: ['', [Validators.required]],
-    });
+      confirmPassword: ['', [Validators.required]]
+    }, { validator: matchingPasswordsValidator('password', 'confirmPassword') });    
   }
 
   onRegister() {
-    if (this.registerForm.valid) {
-      console.log(this.registerForm.value);
-      // Handle registration logic here
-    }
+    console.log('Registering with the following details:');
+      Object.keys(this.registerForm.value).forEach(key => {
+        console.log(`${key}: ${this.registerForm.value[key]}`);
+      });
   }
+
+  formFields = [
+    { label: 'Email cím', controlName: 'email', type: 'email' },
+    { label: 'Felhasználónév', controlName: 'name', type: 'text' },
+    { label: 'Jelszó', controlName: 'password', type: 'password' },
+    { label: 'Jelszó megerősítése', controlName: 'confirmPassword', type: 'password' }
+  ];
+
+  isFieldInvalid(controlName: string): boolean {
+    const control = this.registerForm.get(controlName);
+    return Boolean(control?.invalid && (control?.dirty || control?.touched));
+  }
+
+  getFieldLabel(controlName: string): string {
+    if (this.isFieldInvalid(controlName)) {
+      if(controlName === 'confirmPassword' && !this.isPasswordMatching()){
+        return 'Jelszavak nem egyeznek!'
+      }
+      return `${this.formFields.find(field => field.controlName === controlName)?.label} kötelező!`;
+    }
+    return this.formFields.find(field => field.controlName === controlName)?.label || '';
+  }
+
+  isPasswordMatching(): boolean {
+    const password = this.registerForm.get('password')?.value;
+    const confirmPassword = this.registerForm.get('confirmPassword')?.value;
+
+    return password === confirmPassword;
+  }
+}
+
+export function matchingPasswordsValidator(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+    const control = formGroup.controls[controlName];
+    const matchingControl = formGroup.controls[matchingControlName];
+
+    if (matchingControl.errors && !matchingControl.errors['matchingPasswords']) {
+      return;
+    }
+
+    if (control.value !== matchingControl.value) {
+      matchingControl.setErrors({ matchingPasswords: true });
+    } else {
+      matchingControl.setErrors(null);
+    }
+  };
 }
