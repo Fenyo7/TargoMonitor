58cde484ddbbf5afbf5ab704482ebb63f3fae20c
Fenyo7 <fenyo.lev@gmail.com>
2024-02-26 13:47:39 +0100

    add(fe): working table with data

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { TableColumn, TableRow } from '../table/table.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  columns: TableColumn[] = [
+    { key: 'name', label: 'Név', type: 'text' },
+    { key: 'address', label: 'Cím', type: 'text' },
+    { key: 'hasContract', label: 'Szerződés', type: 'bool' },
+    { key: 'doNotify', label: 'Értesítés', type: 'bool' },
+  ];
 
+  data: TableRow[] = [
+    {
+      id: 1,
+      name: 'Cégnév ide jön',
+      address: 'Cég címe',
+      hasContract: false,
+      doNotify: true,
+    },
+    {
+      id: 2,
+      name: 'Gémesi Kft.',
+      address: 'Tatabánya, Szegfű u. 68.',
+      hasContract: true,
+      doNotify: true,
+    },
+    {
+      id: 3,
+      name: 'Görözdi Kft.',
+      address: 'Ács, Győri u. 29.',
+      hasContract: false,
+      doNotify: false,
+    },
+  ];
 }
--- frontend/src/app/components/table/table.component.ts
+++ frontend/src/app/components/table/table.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+// Interface for column definitions
+export interface TableColumn {
+  key: string; // key of the data object
+  label: string; // display name for the column header
+  type?: 'text' | 'number' | 'date' | 'bool'; // additional types can be added as needed
+  // Add more properties for customization (e.g., format, width)
+}
+
+// Interface for table data
+export interface TableRow {
+  [key: string]: any; // key-value pairs matching the column definitions
+}
 
 @Component({
   selector: 'app-table',
@@ -6,5 +19,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent {
-
+  @Input() clientColumns: TableColumn[] = [];
+  @Input() clientData: TableRow[] = [];
 }
