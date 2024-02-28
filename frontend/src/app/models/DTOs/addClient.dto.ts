2ba3b23de6fd0477d16093a1dfb54be504d70747
Fenyo7 <fenyo.lev@gmail.com>
2024-02-28 21:16:26 +0100

    add(fe): service for handling clients

---
+++ frontend/src/app/models/DTOs/addClient.dto.ts
@@ -0,0 +1,6 @@
+export interface addClientDTO {
+  name: string;
+  address: string;
+  hasContract: boolean;
+  doNotify: boolean;
+}
---
+++ frontend/src/app/services/client.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { addClientDTO } from '../models/DTOs/addClient.dto';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+  private baseUrl = `${environment.apiUrl}/Client`;
+
+  constructor(private http: HttpClient) { }
+
+  addClient(client: addClientDTO): Observable<any> {
+    return this.http.post(this.baseUrl, client);
+  }
+}
