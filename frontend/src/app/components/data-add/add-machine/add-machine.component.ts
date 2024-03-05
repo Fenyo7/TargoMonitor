9073754cf65fb7161e43cace1519d05ad32f5690
Fenyo7 <fenyo.lev@gmail.com>
2024-03-05 15:06:35 +0100

    add(fe): method for generating code for machine kinds

--- frontend/src/app/components/data-add/add-machine/add-machine.component.ts
+++ frontend/src/app/components/data-add/add-machine/add-machine.component.ts
@@ -43,8 +43,9 @@ export class AddMachineComponent implements OnInit {
   isDangerous: boolean = false;
 
   typeOptions = [
+    'Egyéb',
     'Daru',
-    'Önjáró daruk és rakodódaruk',
+    'Önjáró daru / rakodódaru',
     'Targonca',
     'Körülkerített emelőterű emelő',
     'Toronydaru',
@@ -59,7 +60,6 @@ export class AddMachineComponent implements OnInit {
     'Függesztő eszköz',
     'Adapter',
     'Bak',
-    'Egyéb',
   ];
 
   daruOptions = [
@@ -67,107 +67,80 @@ export class AddMachineComponent implements OnInit {
     'Futómacska',
     'Bakdaru',
     'Oszlopos / konzolos forgódaru',
-    'Emelődob'
+    'Emelődob',
   ];
 
-  darulancOptions = [
-    'Sodronyköteles',
-    'Láncos'
-  ];
+  darulancOptions = ['Sodronyköteles', 'Láncos'];
 
   onjaroOptions = [
     'Autódaru',
     'Pókdaru',
     'Rakodódaru (járműre szerelt)',
-    'Földmunkagép daru üzemben'
+    'Földmunkagép daru üzemben',
   ];
 
   targoncaOptions = [
     'Vezetőüléses',
     'Gyalogkíséretű',
     'Vezetőállásos',
-    'Kommissiózó'
+    'Kommissiózó',
   ];
 
-  targoncameghajtOptions = [
-    'Elektromos',
-    'Diesel / Benzin',
-    'Gázüzemű'
-  ];
+  targoncameghajtOptions = ['Elektromos', 'Diesel / Benzin', 'Gázüzemű'];
 
   vezetoulesOptions = [
     'Homlokvillás',
     'Oldalvillás',
     'Tolóoszlopos',
     'Változtatható kinyúlású',
-    'Rakodógép targonca üzemmódban'
+    'Rakodógép targonca üzemmódban',
   ];
 
   mozgomunkaOptions = [
     'Ollós',
     'Karos',
     'Oszlopos',
-    'Rakodógép személyemelő üzemmódban'
+    'Rakodógép személyemelő üzemmódban',
   ];
 
-  mozgomeghajtOptions = [
-    'Elektromos',
-    'Diesel'
-  ];
+  mozgomeghajtOptions = ['Elektromos', 'Diesel'];
 
   jarmureszereltOptions = [
+    'Egyéb',
     'Emelőhátfal',
     'Konténeremelő / multilift',
     'Billencs (nem emelőgép)',
     'Járműre szerelt szerelőkosár',
     'Trailer',
     'Hulladékszállító jármű',
-    'Egyéb'
   ];
 
-  emeloasztalOptions = [
-    'Helyhez kötött',
-    'Mobil'
-  ];
+  emeloasztalOptions = ['Helyhez kötött', 'Mobil'];
 
-  adapterOptions = [
-    'Targoncához',
-    'Daruhoz'
-  ];
+  adapterOptions = ['Targoncához', 'Daruhoz'];
 
   jarmuemeloOptions = [
+    'Egyéb',
     'Kétoszlopos',
     'Négyoszlopos',
     'Padlóemelő',
     'Aknaperememelő',
     'Szintkiegyenlítő',
     'Kézi',
-    'Egyéb'
   ];
 
-  jarmuemelomeghajtOptions = [
-    'Kézi',
-    'Pneumatikus',
-    'Elektromos'
-  ]
+  jarmuemelomeghajtOptions = ['Kézi', 'Pneumatikus', 'Elektromos'];
 
-  kezijarmuemeloOptions = [
-    'Krokodil',
-    'Motorkiemelő (zsiráf)',
-    'Olajemelő'
-  ]
+  kezijarmuemeloOptions = ['Krokodil', 'Motorkiemelő (zsiráf)', 'Olajemelő'];
 
   keziemeloOptions = [
+    'Egyéb',
     'Kézi magasemelésű targonca',
     'Kézi csörlő',
     'Emelőasztal',
-    'Egyéb'
   ];
 
-  vezerlesOptions = [
-    'Talajról vezérelt',
-    'Rádió távvezérlés'
-  ];
+  vezerlesOptions = ['Talajról vezérelt', 'Rádió távvezérlés'];
 
   constructor(
     private fb: FormBuilder,
@@ -205,7 +178,7 @@ export class AddMachineComponent implements OnInit {
       load: [''],
       span: [''],
       rope: [''],
-      bend: ['']
+      bend: [''],
     });
 
     this.clients$ = this.clientService.getAllClients();
@@ -233,11 +206,208 @@ export class AddMachineComponent implements OnInit {
     this[field] = !this[field];
   }
 
-  generateCode() {
-    
+  generateCode(): string | null {
+    let code: string = '';
+    let typeCode = 0;
+    if (this.typeFormControl.value) {
+      typeCode = this.typeOptions.indexOf(this.typeFormControl.value);
+      if (typeCode / 10 < 1) {
+        code += `0${typeCode}.`;
+      } else {
+        code += typeCode + '.';
+      }
+    } else return null;
+
+    switch (typeCode) {
+      case 1: {
+        // Daru
+        let daruCode = 0;
+        if (this.daruControl.value) {
+          daruCode = this.daruOptions.indexOf(this.daruControl.value);
+          if (daruCode / 10 < 1) {
+            code += `0${daruCode}.`;
+          } else {
+            code += daruCode + '.';
+          }
+        } else return null;
+
+        let darulancCode = 0;
+        if (this.darulancControl.value) {
+          darulancCode = this.darulancOptions.indexOf(
+            this.darulancControl.value
+          );
+          if (darulancCode / 10 < 1) {
+            code += `0${darulancCode}.`;
+          } else {
+            code += darulancCode + '.';
+          }
+        } else return null;
+        break;
+      }
+      case 2: {
+        // Önjáró daru / rakodódaru
+        let onjaroCode = 0;
+        if (this.onjaroControl.value) {
+          onjaroCode = this.onjaroOptions.indexOf(this.onjaroControl.value);
+          if (onjaroCode / 10 < 1) {
+            code += `0${onjaroCode}.`;
+          } else {
+            code += onjaroCode + '.';
+          }
+        } else return null;
+        break;
+      }
+      case 3: {
+        // Targonca
+        let targoncameghajtCode = 0;
+        if (this.targoncameghajtControl.value) {
+          targoncameghajtCode = this.targoncameghajtOptions.indexOf(
+            this.targoncameghajtControl.value
+          );
+          if (targoncameghajtCode / 10 < 1) {
+            code += `0${targoncameghajtCode}.`;
+          } else {
+            code += targoncameghajtCode + '.';
+          }
+        } else return null;
+
+        let targoncaCode = 0;
+        if (this.targoncaControl.value) {
+          targoncaCode = this.targoncaOptions.indexOf(
+            this.targoncaControl.value
+          );
+          if (targoncaCode / 10 < 1) {
+            code += `0${targoncaCode}.`;
+          } else {
+            code += targoncaCode + '.';
+          }
+        } else return null;
+
+        let vezetoulesCode = 0;
+        if (targoncaCode === 0) {
+          if (this.vezetoulesControl.value) {
+            vezetoulesCode = this.vezetoulesOptions.indexOf(
+              this.vezetoulesControl.value
+            );
+            if (vezetoulesCode / 10 < 1) {
+              code += `0${vezetoulesCode}.`;
+            } else {
+              code += vezetoulesCode + '.';
+            }
+          } else return null;
+        }
+        break;
+      }
+      case 7: {
+        // Járműre szerelt emelőgép
+        let jarmureszereltCode = 0;
+        if (this.jarmureszereltControl.value) {
+          jarmureszereltCode = this.jarmureszereltOptions.indexOf(
+            this.jarmureszereltControl.value
+          );
+          if (jarmureszereltCode / 10 < 1) {
+            code += `0${jarmureszereltCode}.`;
+          } else {
+            code += jarmureszereltCode + '.';
+          }
+        } else return null;
+        break;
+      }
+      case 8: {
+        // Járműemelő
+        let jarmuemelomeghajtCode = 0;
+        if (this.jarmuemelomeghajtControl.value) {
+          jarmuemelomeghajtCode = this.jarmuemelomeghajtOptions.indexOf(
+            this.jarmuemelomeghajtControl.value
+          );
+          if (jarmuemelomeghajtCode / 10 < 1) {
+            code += `0${jarmuemelomeghajtCode}.`;
+          } else {
+            code += jarmuemelomeghajtCode + '.';
+          }
+        } else return null;
+
+        let jarmuemeloCode = 0;
+        if (this.jarmuemeloControl.value) {
+          jarmuemeloCode = this.jarmuemeloOptions.indexOf(
+            this.jarmuemeloControl.value
+          );
+          if (jarmuemeloCode / 10 < 1) {
+            code += `0${jarmuemeloCode}.`;
+          } else {
+            code += jarmuemeloCode + '.';
+          }
+        } else return null;
+
+        let keziemeloCode = 0;
+        if (jarmuemeloCode === 6) {
+          if (this.kezijarmuemeloControl.value) {
+            keziemeloCode = this.kezijarmuemeloOptions.indexOf(
+              this.kezijarmuemeloControl.value
+            );
+            if (keziemeloCode / 10 < 1) {
+              code += `0${keziemeloCode}.`;
+            } else {
+              code += keziemeloCode + '.';
+            }
+          } else return null;
+        }
+        break;
+      }
+      case 9: {
+        // Emelőasztal
+        let emeloasztalCode = 0;
+        if (this.emeloasztalControl.value) {
+          emeloasztalCode = this.emeloasztalOptions.indexOf(
+            this.emeloasztalControl.value
+          );
+          if (emeloasztalCode / 10 < 1) {
+            code += `0${emeloasztalCode}.`;
+          } else {
+            code += emeloasztalCode + '.';
+          }
+        } else return null;
+        break;
+      }
+      case 10: {
+        // Kézi emelőeszköz
+        let keziemeloCode = 0;
+        if (this.keziemeloControl.value) {
+          keziemeloCode = this.keziemeloOptions.indexOf(
+            this.keziemeloControl.value
+          );
+          if (keziemeloCode / 10 < 1) {
+            code += `0${keziemeloCode}.`;
+          } else {
+            code += keziemeloCode + '.';
+          }
+        } else return null;
+        break;
+      }
+      case 15: {
+        // Adapter
+        let adapcerCode = 0;
+        if (this.adapterControl.value) {
+          adapcerCode = this.adapterOptions.indexOf(
+            this.adapterControl.value
+          );
+          if (adapcerCode / 10 < 1) {
+            code += `0${adapcerCode}.`;
+          } else {
+            code += adapcerCode + '.';
+          }
+        } else return null;
+        break;
+      }
+    }
+
+    return code;
   }
 
   onSubmit() {
+    const code = this.generateCode();
+
+    /*
     if(this.machineForm.valid) {
       const selectedClient = this.machineForm.value.client as Client;
 
@@ -250,6 +420,6 @@ export class AddMachineComponent implements OnInit {
         next: (response) => console.log(response),
         error: (error) => console.log(error),
       });
-    }
+    }*/
   }
 }
